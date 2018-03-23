5269aa4b0b8a1b40eb83f50e188713dff634c392
Unknown <luis.ycaza@gmail.com>
2018-03-23 18:36:31 -0500

    Proyecto: commit inicial

---
+++ portal/views.py
@@ -0,0 +1,17 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Prueba de funcionamiento")
+    
+
+
+
+
+
+
